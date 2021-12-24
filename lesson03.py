306aff6f4a5fe98837f1c3a656c580f08cc0836e
Alex95IK <alex95i.k.7@gmail.com>
2021-12-24 22:57:26 +0200

    first commit

---
+++ lesson03.py
@@ -0,0 +1,32 @@
+first_name = 'Mike'
+last_name = 'Forest'
+age = 20
+print("Hi! My name is " + first_name + " " + last_name + ", I`m " + str(age) + " years old.")
+
+print('/t Baa, baa, black sheep,
+
+Have you any wool?
+
+Yes sir, yes sir,
+
+Three bags full
+
+
+
+One for the master,
+
+One for the dame,
+
+And one for the little boy
+
+Who lives down the lane
+
+
+
+Baa, baa, black sheep,
+
+Have you any wool?
+
+Yes sir, yes sir,
+
+Three bags full')
