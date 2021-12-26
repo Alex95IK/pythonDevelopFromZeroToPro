cfed9d218d9e449974acbb9359258a704f8db6db
Alex95IK <alex95i.k.7@gmail.com>
2021-12-26 17:28:08 +0200

    Finished lesson 16 and exercise 06

---
+++ Exersice06.py
@@ -0,0 +1,8 @@
+# Ex 01
+print('Hello Python!'[6] + 'a' + 'Hello Python!'[8:10])
+
+# Ex 02
+print('z' * 7)
+
+# Ex 03
+print('z'.upper() * 7)
---
+++ lesson16.py
@@ -0,0 +1,19 @@
+yummy = 'This is something text '
+
+# MULTIPLICATION
+print(yummy * 2)
+
+# UPPER REGISTER METHOD
+print(yummy.upper())
+# or
+print('Something text '.upper())
+
+# LOWER REGISTER METHOD
+print(yummy.lower())
+# or
+print('Something text '.lower())
+
+# SPLIT METHOD
+print(yummy.split())
+# or we can give custom split sign
+print(yummy.split('s'))
