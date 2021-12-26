0756b5dc07bad5d919a7826f38decdc24f71dc39
Alex95IK <alex95i.k.7@gmail.com>
2021-12-26 16:13:32 +0200

    corrected test number four

--- Exercise04.py
+++ Exercise04.py
@@ -6,6 +6,9 @@ print('Hello Python!'[3])
 print(text[:2])
 print(text[:2:1])
 
+# Incorrect!:
 new_text = text[8:10]
-
 print('Pa' + new_text)
+
+# Correct!:
+print('Hello Python!'[6:7] + 'a' + 'Hello Python!'[8:10])
