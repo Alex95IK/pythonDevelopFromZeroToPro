a8fb7aeaa14455f1b62052e5a41d89f2f5f3e898
Alex95IK <alex95i.k.7@gmail.com>
2021-12-31 00:48:18 +0200

    Exercise 23 Booleans and Exercise 23

---
+++ Exercise__23__Booleans.py
@@ -0,0 +1,10 @@
+test_one = 20
+test_two = 57
+
+print(test_one == test_two)
+print(test_one != test_two)
+print(test_one <= test_two)
+print(test_one >= test_two)
+
+print('A' > 'a')
+print(ord('A'), ord('a'))
---
+++ Lesson__23__Booleans.py
@@ -0,0 +1,22 @@
+test_one = 5
+test_two = 7
+
+print(test_one == test_two)
+print(test_one != test_two)
+print(test_one < test_two)
+print(test_one > test_two)
+print(test_one <= test_two)
+print(test_one >= test_two)
+
+str_one = 'some text'
+str_two = 'SOME TEXT'
+
+print(str_one == str_two)  # Return the FALSE!
+
+print(ord(str_one))  # Function for showing "ASCII" Code
+print(ord(str_two))  # Function for showing "ASCII" Code
+
+one = input('Input first number')
+two = input('Input second number')
+
+print(one < two)
